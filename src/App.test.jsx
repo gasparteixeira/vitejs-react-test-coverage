2e6ec170f5b0cb183292292359ac139b1e2f5fc4
CodeSandbox <support@codesandbox.io>
2023-10-06 09:07:48 +0000

    Initial commit

---
+++ src/App.test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App />);
+    expect(screen.getByText("React")).toBeInTheDocument();
+  });
+});
